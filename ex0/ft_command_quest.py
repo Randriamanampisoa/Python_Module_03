c47d8a2f1461d528868e1d08b7da610a15c2b468
fanilran <fanilran@student.42antananari>
2026-05-13 17:40:23 +0300

    Module03: ex0

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   ft_command_quest.py                                  :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: fanilran <fanilran@student.42.fr>            +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/04/28 15:17:55 by fanilran            #+#    #+#            #
+#   Updated: 2026/04/28 19:58:01 by fanilran           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+import sys
+
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    print("Program name:", sys.argv[0])
+    i = 1
+    if len(sys.argv) == 1:
+        print("No arguments provided!")
+    else:
+        print("Arguments received: ", len(sys.argv) - 1)
+        for x in sys.argv[1:]:
+            print(f"Argument {i}: {x}")
+            i += 1
+    print("Total arguments:", len(sys.argv))
