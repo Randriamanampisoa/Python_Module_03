e44318d8774f035743e37371b780ba935e01f769
fanilran <fanilran@student.42antananari>
2026-05-17 07:58:48 +0300

    feat: Modification for the ft_inventory_system.py

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -7,7 +7,7 @@
 #   By: fanilran <fanilran@student.42.fr>            +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/05/14 12:25:37 by fanilran            #+#    #+#            #
-#   Updated: 2026/05/14 16:16:28 by fanilran           ###   ########.fr      #
+#   Updated: 2026/05/17 07:57:01 by fanilran           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -35,13 +35,32 @@ def parse(args: list):
 def main():
     args = sys.argv[1:]
     tmp = parse(args)
-
     print(f"Got inventory: {tmp}")
     lst = list(tmp.keys())
-    len_lst_keys = len(lst)
+    len_keys = len(lst)
     values = sum(tmp.values())
     print(f"Item list: {lst}")
-    print(f"Total quantity of the {len_lst_keys} items: {values}")
+    print(f"Total quantity of the {len_keys} items: {values}")
+    for key in tmp:
+        value = tmp[key]
+        percentage = (value / values) * 100
+        print(f"Item {key} represents {round(percentage, 1)}%")
+    max_val = 0
+    max_key = None
+    for v in tmp:
+        if tmp[v] > max_val:
+            max_val = tmp[v]
+            max_key = v
+    min_key = lst[0]
+    min_val = tmp[min_key]
+    for val in tmp:
+        if tmp[val] < min_val:
+            min_val = tmp[val]
+            min_key = val
+    print(f"Item most abundant: {max_key} with quantity {max_val}")
+    print(f"Item most abundant: {min_key} with quantity {min_val}")
+    tmp.update({"magic_item":1})
+    print(f"Updated inventory: {tmp}")
 
 if __name__ == "__main__":
     print("=== Inventory System Analysis ===")
